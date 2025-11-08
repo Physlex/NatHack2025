2d4bcf9bc5a04b8de67e5e00aeaf5215379afcf7
Ayrton Chilibeck <git@blobfish.icu>
2025-11-08 01:00:11 -0700

    task: check for openocd
    task: cmsis dsp lib includes

--- activate.sh
+++ activate.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-set -e  # Exit on error
-
 VENDOR_DIR="vendor"
 TOOLCHAIN_DIR="$VENDOR_DIR/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi"
 GCC_BIN="$TOOLCHAIN_DIR/bin/arm-none-eabi-gcc"
@@ -16,6 +14,13 @@ fi
 
 # Add toolchain to PATH
 export PATH="$PATH:$(pwd)/$TOOLCHAIN_DIR/bin"
+
+# Check if OpenOCD is installed
+OPENOCD=$(which openocd)
+if [ -z "$OPENOCD" ]; then
+    echo "OpenOCD not found. Please install OpenOCD."
+    exit 1
+fi
 
 echo "ARM GNU Toolchain is ready to use."
 echo "Current PATH includes: $(pwd)/$TOOLCHAIN_DIR/bin"
