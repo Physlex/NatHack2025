03cba4a92e9d6cfa77dff0aead1ab08e0cb4b934
Ayrton Chilibeck <git@blobfish.icu>
2025-11-07 21:10:41 -0700

    task: Gitignore vendor

--- app-bbox/get_gcc.sh
+++ app-bbox/get_gcc.sh
@@ -1,5 +1,23 @@
-wget -P vendor/arm.tar.xz https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi.tar.xz
+#!/bin/bash
 
-tar -xz vendor/arm.tar.xz
+set -e  # Exit on error
 
-export PATH="$PATH:vendor/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi/bin"
+TOOLCHAIN_URL="https://developer.arm.com/-/media/Files/downloads/gnu/14.3.rel1/binrel/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi.tar.xz"
+ARCHIVE_NAME="arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi.tar.xz"
+VENDOR_DIR="vendor"
+
+echo "Downloading ARM GNU Toolchain..."
+wget -O "$ARCHIVE_NAME" "$TOOLCHAIN_URL"
+
+echo "Creating vendor directory..."
+mkdir -p "$VENDOR_DIR"
+
+echo "Extracting toolchain to vendor directory..."
+tar -xJf "$ARCHIVE_NAME" -C "$VENDOR_DIR"
+
+echo "Cleaning up archive..."
+rm "$ARCHIVE_NAME"
+
+echo "ARM GNU Toolchain installed successfully in $VENDOR_DIR/"
+echo "To use the toolchain, add the following to your PATH:"
+echo "export PATH=\"\$PATH:\$(pwd)/$VENDOR_DIR/arm-gnu-toolchain-14.3.rel1-x86_64-arm-none-eabi/bin\""
