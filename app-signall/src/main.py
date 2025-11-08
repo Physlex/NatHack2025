07bc4bbfc06125c8d912e4d390e2e8e6b2e0f915
Balpreet Singh <bjuneja@ualberta.ca>
2025-11-08 15:43:05 -0700

    .

--- app-signall/src/main.py
+++ app-signall/src/main.py
@@ -1,5 +1,8 @@
 import serial
+from serial import SerialException
 import time
+import struct
+import sys
 
 import serial.tools.list_ports
 '''
@@ -8,16 +11,24 @@ for p in ports:
 
     print(f"{p.device}\t{p.description}\t{p.hwid}")
     '''
-    
-with serial.Serial('COM9', 9600, timeout=1) as ser:
-    print(ser.name)
-    command = int(input("Read(1) or Write(2): "))
-    if command == 1:
-        x = ser.read()          # read one byte
-        print(x)
-    if command == 2:
-        msg = input("Enter what you want to write: ")
-        ser.write(msg.encode('utf-8'))
+
+PORT_NAME = 'COM9'
+
+try:
+    with serial.Serial(PORT_NAME, 9600, timeout=1) as ser:
+        print(ser.name, ser.port)
+        command = int(input("Read(1) or Write(2): "))
+        if command == 1:
+            bitcommand = 0x01
+            x = ser.read()          # read one byte
+            print(x)
+        if command == 2:
+            bitcommand = 0x02
+            msg = input("Enter what you want to write: ")
+            ser.write(msg.encode('utf-8'))
+except SerialException:
+    print("UART not connected yet")
         
+    
     #s = ser.read(10)        # read up to ten bytes (timeout)
     #line = ser.readline()
