f65803f621742663db3b2812a526e047438e9d43
Balpreet Singh <bjuneja@ualberta.ca>
2025-11-09 23:14:06 -0700

    added foreign key to session

--- app-backend/mysite/store/models.py
+++ app-backend/mysite/store/models.py
@@ -5,6 +5,8 @@ from django.db import models
 class RecordingSession(models.Model):
     id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=100)
+    user = models.ForeignKey('auth.User', on_delete=models.CASCADE, null=True, blank=True)
+    
 class TimeSeriesEntry(models.Model):
     timestamp = models.DateTimeField(auto_now_add=True, primary_key=True)
     session = models.ForeignKey(RecordingSession, on_delete=models.CASCADE, related_name='entries', default=None)
