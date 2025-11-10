519938ca85fbca7c3119d1ca3b40fa1e08b8aeb7
Balpreet Singh <bjuneja@ualberta.ca>
2025-11-09 20:58:54 -0700

    update urls again

--- app-backend/mysite/store/urls.py
+++ app-backend/mysite/store/urls.py
@@ -4,8 +4,8 @@ from . import views
 app_name = "store"
 
 urlpatterns = [
-    # GET/POST /store/endpoint/
-    
+    # GET: /store/endpoint/<sid>/ - Get data for a specific session
+    # POST: /store/endpoint/ - Create new time series entries
     path("endpoint/<int:sid>/", views.TimeSeriesEndpointView.as_view(), name="endpoint_get"),
-    path("endpoint/create/", views.TimeSeriesEndpointView.as_view(), name="endpoint_create"),
+    path("endpoint/", views.TimeSeriesEndpointView.as_view(), name="endpoint_create"),
 ]
