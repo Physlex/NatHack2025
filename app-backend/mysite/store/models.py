a8eeaa850c1be71204159fa4c8cbaabfef5f2cc5
pr0d1gypumkn <akeshta@ualberta.ca>
2025-11-10 02:55:13 -0700

    wowzas

--- app-backend/mysite/store/models.py
+++ app-backend/mysite/store/models.py
@@ -1,10 +1,11 @@
 from django.db import models
 from django.conf import settings
+import uuid
 
 # Create your models here.
 
 class RecordingSession(models.Model):
-    id = models.UUIDField(primary_key=True)
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=100)
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     
