28c12ec897cd5b813056fa19e02f9abe9ad691ac
Tammy Young <tammy.q.young@gmail.com>
2025-11-09 17:48:36 -0700

    feat: add flowbite config and update login page

--- app-frontend/src/pages/home.jsx
+++ app-frontend/src/pages/home.jsx
@@ -1,8 +1,13 @@
 import { useNavigate } from "react-router-dom";
-import { Button, Card, Checkbox, Label, TextInput } from "flowbite-react";
+import { Button, Checkbox, Label, TextInput } from "flowbite-react";
+import { useState } from "react";
 
 export default function Home() {
   const navigate = useNavigate();
+  const [data, setData] = useState({
+    email: "",
+    password: ""
+  })
 
   function login() {
     navigate('/dashboard');
@@ -10,7 +15,7 @@ export default function Home() {
 
   return (
     <div className="bg-primary-light/10 w-screen min-h-lvh flex items-center justify-center grid grid-cols-2">
-      <img src="/assets/images/background-lines-blue.png" alt="background-lines" className="w-full h-full fixed -left-1/4 -top-1/6 opacity-20" />
+      <img src="/assets/images/background-lines-blue.png" alt="background-lines" className="w-full h-full fixed -left-1/4 -top-1/6 opacity-50" />
       <div className="col-span-1 h-full" />
       <div className="col-span-1 flex flex-col items-center justify-center px-12 bg-white z-10 h-full">
         <h1 className="!text-3xl font-bold mb-6 text-center">Sign In</h1>
@@ -22,7 +27,8 @@ export default function Home() {
             <TextInput
               id="email"
               type="email"
-              placeholder="name@flowbite.com"
+              placeholder="name@example.com"
+              onChange={(e) => setData((prev) => ({ ...prev, email: e.target.value }))}
               required
             />
           </div>
@@ -33,6 +39,8 @@ export default function Home() {
             <TextInput
               id="password"
               type="password"
+              placeholder="Password"
+              onChange={(e) => setData((prev) => ({ ...prev, password: e.target.value }))}
               required
             />
           </div>
@@ -40,7 +48,7 @@ export default function Home() {
             <Checkbox id="remember" />
             <p className="!my-0 !py-0 !text-gray-600">Remember Me</p>
           </div>
-          <Button type="submit" onClick={login} className="cursor-pointer">
+          <Button type="submit" onClick={login} className="cursor-pointer bg-primary transition-all duration-200">
             Submit
           </Button>
         </form>
