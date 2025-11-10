7b390764c953b3aee56f670d712f0d5dbde90b8c
pr0d1gypumkn <akeshta@ualberta.ca>
2025-11-10 09:57:35 -0700

    .

--- app-backend/mysite/store/urls.py
+++ app-backend/mysite/store/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path("sessions/<uuid:uid>/", views.SessionsByUserView.as_view(), name="session_list"),
     path("spectrogram/<int:sid>/", views.SpectrogramView.as_view(), name="spectrogram"),
     path("spectrogram/<int:sid>/erps/", views.ERPView.as_view(), name="erps"),
+    path("spectrogram/<int:sid>/mfcc/", views.MFCCView.as_view(), name="mfcc"),
 ]
