d8df96174183524e7bfa24c24536cdcaca5d8eec
pr0d1gypumkn <akeshta@ualberta.ca>
2025-11-09 21:45:48 -0700

    registered user model

--- app-backend/mysite/bboxauth/admin.py
+++ app-backend/mysite/bboxauth/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import CustomUser
 
 # Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_active', 'is_staff')
+    search_fields = ('username', 'email')
+    readonly_fields = ('date_joined', 'last_login')
