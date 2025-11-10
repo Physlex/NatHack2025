d2f1b8ecb2d198bfa0fd90d4a34eeabb1c9f8b9e
Tammy Young <tammy.q.young@gmail.com>
2025-11-09 22:17:33 -0700

    feat: add logo to login page

--- app-frontend/src/pages/home.jsx
+++ app-frontend/src/pages/home.jsx
@@ -62,123 +62,126 @@ export default function Home() {
     <div className="bg-primary-light/10 w-screen min-h-lvh flex items-center justify-center grid grid-cols-2">
       <img src="/assets/images/background-lines-blue.png" alt="background-lines" className="w-full h-full fixed -left-1/4 -top-1/6 opacity-50" />
       <div className="col-span-1 h-full" />
-      {
-        mode === "login" ? (
-          <div className="col-span-1 flex flex-col items-center justify-center px-12 bg-white z-10 h-full">
-            <h1 className="!text-3xl font-bold mb-6 text-center">Sign In</h1>
-            {
-              errors.detail && <HelperText color="failure">{errors.detail}</HelperText>
-            }
-            <form className="flex flex-col gap-4 w-1/2">
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="email" value="Email">Email</Label>
+      <div className="col-span-1 flex flex-col items-center justify-center px-12 bg-white z-10 h-full w-full">
+        <img src="/assets/images/brain-box-icon.png" alt="Brain Box" className="w-36 h-36 mb-4 -mt-32" />
+        {
+          mode === "login" ? (
+            <div className="w-1/2">
+              <h1 className="!text-3xl font-bold mb-6 text-center">Sign In</h1>
+              {
+                errors.detail && <HelperText color="failure">{errors.detail}</HelperText>
+              }
+              <form className="flex flex-col gap-4">
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="email" value="Email">Email</Label>
+                  </div>
+                  <TextInput
+                    id="email"
+                    type="email"
+                    placeholder="name@example.com"
+                    value={data.email}
+                    onChange={(e) => setData((prev) => ({ ...prev, email: e.target.value }))}
+                    required
+                  />
+                  {
+                    errors.email && <HelperText color="failure">{errors.email}</HelperText>
+                  }
                 </div>
-                <TextInput
-                  id="email"
-                  type="email"
-                  placeholder="name@example.com"
-                  value={data.email}
-                  onChange={(e) => setData((prev) => ({ ...prev, email: e.target.value }))}
-                  required
-                />
-                {
-                  errors.email && <HelperText color="failure">{errors.email}</HelperText>
-                }
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="password" value="Password">Password</Label>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="password" value="Password">Password</Label>
+                  </div>
+                  <TextInput
+                    id="password"
+                    type="password"
+                    value={data.password}
+                    placeholder="Password"
+                    onChange={(e) => setData((prev) => ({ ...prev, password: e.target.value }))}
+                    required
+                  />
+                  {
+                    errors.password && <HelperText color="failure">{errors.password}</HelperText>
+                  }
                 </div>
-                <TextInput
-                  id="password"
-                  type="password"
-                  value={data.password}
-                  placeholder="Password"
-                  onChange={(e) => setData((prev) => ({ ...prev, password: e.target.value }))}
-                  required
-                />
-                {
-                  errors.password && <HelperText color="failure">{errors.password}</HelperText>
-                }
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <Checkbox id="remember" />
-                <p className="!my-0 !py-0 !text-gray-600">Remember Me</p>
-              </div>
-              <Button type="submit" onClick={onSubmit} className="cursor-pointer bg-primary transition-all duration-200">
-                Sign In
-              </Button>
-              <p>Don't have an account? <span className="text-primary font-medium cursor-pointer" onClick={() => changeMode("signup")}>Sign Up</span></p>
-            </form>
-          </div>
-        ) : (
-          <div className="col-span-1 flex flex-col items-center justify-center px-12 bg-white z-10 h-full">
-            <h1 className="!text-3xl font-bold mb-6 text-center">Sign Up</h1>
-            {
-              errors.detail && <HelperText color="failure">{errors.detail}</HelperText>
-            }
-            <form className="flex flex-col gap-4 w-1/2">
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="name">Name</Label>
+                <div className="flex items-center gap-2 w-full">
+                  <Checkbox id="remember" />
+                  <p className="!my-0 !py-0 !text-gray-600">Remember Me</p>
                 </div>
-                <TextInput
-                  id="name"
-                  type="text"
-                  value={data.name}
-                  placeholder="Your full name"
-                  onChange={(e) => setData((prev) => ({ ...prev, name: e.target.value }))}
-                  required
-                />
-                {
-                  errors.name && <HelperText color="failure">{errors.name}</HelperText>
-                }
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="email" value="Email">Email</Label>
+                <Button type="submit" onClick={onSubmit} className="cursor-pointer bg-primary transition-all duration-200">
+                  Sign In
+                </Button>
+                <p>Don't have an account? <span className="text-primary font-medium cursor-pointer" onClick={() => changeMode("signup")}>Sign Up</span></p>
+              </form>
+            </div>
+          ) : (
+            <div className="w-1/2">
+              <h1 className="!text-3xl font-bold mb-6 text-center">Sign Up</h1>
+              {
+                errors.detail && <HelperText color="failure">{errors.detail}</HelperText>
+              }
+              <form className="flex flex-col gap-4">
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="name">Name</Label>
+                  </div>
+                  <TextInput
+                    id="name"
+                    type="text"
+                    value={data.name}
+                    placeholder="Your full name"
+                    onChange={(e) => setData((prev) => ({ ...prev, name: e.target.value }))}
+                    required
+                  />
+                  {
+                    errors.name && <HelperText color="failure">{errors.name}</HelperText>
+                  }
                 </div>
-                <TextInput
-                  id="email"
-                  type="email"
-                  value={data.email}
-                  placeholder="name@example.com"
-                  onChange={(e) => setData((prev) => ({ ...prev, email: e.target.value }))}
-                  required
-                />
-                {
-                  errors.email && <HelperText color="failure">{errors.email}</HelperText>
-                }
-              </div>
-              <div>
-                <div className="mb-2 block">
-                  <Label htmlFor="password">Password</Label>
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="email" value="Email">Email</Label>
+                  </div>
+                  <TextInput
+                    id="email"
+                    type="email"
+                    value={data.email}
+                    placeholder="name@example.com"
+                    onChange={(e) => setData((prev) => ({ ...prev, email: e.target.value }))}
+                    required
+                  />
+                  {
+                    errors.email && <HelperText color="failure">{errors.email}</HelperText>
+                  }
                 </div>
-                <TextInput
-                  id="password"
-                  type="password"
-                  value={data.password}
-                  placeholder="Password"
-                  onChange={(e) => setData((prev) => ({ ...prev, password: e.target.value }))}
-                  required
-                />
-                {
-                  errors.password && <HelperText color="failure">{errors.password}</HelperText>
-                }
-              </div>
-              <div className="flex items-center gap-2 w-full">
-                <Checkbox id="remember" />
-                <p className="!my-0 !py-0 !text-gray-600">Remember Me</p>
-              </div>
-              <Button type="submit" onClick={onSubmit} className="cursor-pointer bg-primary transition-all duration-200">
-                Sign Up
-              </Button>
-              <p>Have an account? <span className="text-primary font-medium cursor-pointer" onClick={() => changeMode("login")}>Sign In</span></p>
-            </form>
-          </div>
-        )
-      }
+                <div>
+                  <div className="mb-2 block">
+                    <Label htmlFor="password">Password</Label>
+                  </div>
+                  <TextInput
+                    id="password"
+                    type="password"
+                    value={data.password}
+                    placeholder="Password"
+                    onChange={(e) => setData((prev) => ({ ...prev, password: e.target.value }))}
+                    required
+                  />
+                  {
+                    errors.password && <HelperText color="failure">{errors.password}</HelperText>
+                  }
+                </div>
+                <div className="flex items-center gap-2 w-full">
+                  <Checkbox id="remember" />
+                  <p className="!my-0 !py-0 !text-gray-600">Remember Me</p>
+                </div>
+                <Button type="submit" onClick={onSubmit} className="cursor-pointer bg-primary transition-all duration-200">
+                  Sign Up
+                </Button>
+                <p>Have an account? <span className="text-primary font-medium cursor-pointer" onClick={() => changeMode("login")}>Sign In</span></p>
+              </form>
+            </div>
+          )
+        }
+      </div>
     </div>
   )
 }
